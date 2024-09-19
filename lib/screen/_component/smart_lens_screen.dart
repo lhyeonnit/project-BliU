a807e28c3c124b2b536c86e384f368e5e56dd942
hyeonnit <lhyeonnit@gmail.com>
2024-09-19 18:14:28 +0900

    search 수정

--- lib/screen/_component/smart_lens_screen.dart
+++ lib/screen/_component/smart_lens_screen.dart
@@ -1,1 +1,16 @@
 //스마트 렌즈
+import 'package:flutter/material.dart';
+
+class SmartLensScreen extends StatefulWidget {
+  const SmartLensScreen({super.key});
+
+  @override
+  State<SmartLensScreen> createState() => _SmartLensScreenState();
+}
+
+class _SmartLensScreenState extends State<SmartLensScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
