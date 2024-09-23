05566d5bd5014fdead963569e8dee22aa78d14bf
lhyeonnit <lhyeonnit@gmail.com>
2024-09-23 15:04:09 +0900

    product review

--- lib/screen/product/component/detail/product_review_list.dart
+++ lib/screen/product/component/detail/product_review_list.dart
@@ -77,163 +77,173 @@ class _ProductReviewState extends ConsumerState<ProductReview> {
         final starAvg = model?.reviewInfoResponseDTO?.reviewInfo?.starAvg ?? 0.0;
         final reviewCount = model?.reviewInfoResponseDTO?.reviewInfo?.reviewCount ?? 0;
 
-        return ListView(
-          children: [
-            // 평점과 총 리뷰 수 섹션
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              margin: EdgeInsets.only(top: 30),
-              child: Row(
-                children: [
-                  Text(
-                    '$starAvg',
-                    style: TextStyle(
-                      fontSize: Responsive.getFont(context, 20),
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                    child: Text(
-                      '/5.0',
+        return Container(
+          margin: EdgeInsets.only(bottom: 80),
+          child: ListView(
+            children: [
+              // 평점과 총 리뷰 수 섹션
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                margin: EdgeInsets.only(top: 30),
+                child: Row(
+                  children: [
+                    Text(
+                      '$starAvg',
                       style: TextStyle(
                         fontSize: Responsive.getFont(context, 20),
                         fontWeight: FontWeight.bold,
-                        color: Color(0xFFA4A4A4),
+                        color: Colors.black,
                       ),
                     ),
-                  ),
-                  const Spacer(),
-                  Text(
-                    '$reviewCount명의 리뷰',
-                    style: TextStyle(
-                      fontSize: Responsive.getFont(context, 14),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-             // TODO 실제 별점 값 넣기
-             Container(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-                child: _ratingTotalStars(4.5),
-              ),
-            Container(
-              margin: EdgeInsets.only(top: 10, bottom: 20),
-              height: 10,
-              width: double.infinity,
-              color: Color(0xFFF5F9F9),
-            ),
-
-            // 리뷰 리스트
-            ...List.generate(list.length, (index) {
-              // TODO 이미지 파싱 작업 필요
-              List<String> reviewImages = [
-                'assets/images/home/exhi.png',
-                'assets/images/home/exhi.png',
-                'assets/images/home/exhi.png',
-                'assets/images/home/exhi.png',
-              ];
-              final reviewData = list[index];
-
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0), // 양옆 여백 추가
-                child: GestureDetector(
-                  onTap: () {
-                    // 각 리뷰를 클릭했을 때 리뷰 상세 페이지로 이동
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const ProductReviewDetail(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                      child: Text(
+                        '/5.0',
+                        style: TextStyle(
+                          fontSize: Responsive.getFont(context, 20),
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFFA4A4A4),
+                        ),
                       ),
-                    );
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
+                    ),
+                    const Spacer(),
+                    Text(
+                      '$reviewCount명의 리뷰',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: Responsive.getFont(context, 14),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+               // TODO 실제 별점 값 넣기
+               Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                  child: _ratingTotalStars(4.5),
+                ),
+              Container(
+                margin: EdgeInsets.only(top: 10, bottom: 20),
+                height: 10,
+                width: double.infinity,
+                color: Color(0xFFF5F9F9),
+              ),
+
+              // 리뷰 리스트
+              ...List.generate(list.length, (index) {
+                // TODO 이미지 파싱 작업 필요
+                List<String> reviewImages = [
+                  'assets/images/home/exhi.png',
+                  'assets/images/home/exhi.png',
+                  'assets/images/home/exhi.png',
+                  'assets/images/home/exhi.png',
+                ];
+                final reviewData = list[index];
+
+                return GestureDetector(
+                    onTap: () {
+                      // 각 리뷰를 클릭했을 때 리뷰 상세 페이지로 이동
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const ProductReviewDetail(),
+                        ),
+                      );
+                    },
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              '${reviewData.mtId.toString().substring(0, 2)}**********',
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black
-                              ),
-                            ),
-                            Text(
-                              reviewData.rtWdate ?? "",
-                              style: const TextStyle(color: Colors.grey)
-                            ),
-                          ],
-                        ),
-                        // TODO 실제 별점 값 넣기
-                        Container(
-                          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-                          child: _ratingStars(4.5),
-                        ),
-                        const SizedBox(height: 8),
-                        // 리뷰 텍스트 두 줄로 제한하고 넘칠 경우 생략 부호 표시
-                        Text(
-                          reviewData.rtContent ?? "",
-                          style: TextStyle(
-                            fontSize: Responsive.getFont(context, 14),
-                            color: Colors.black
-                          ),
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        const SizedBox(height: 8),
-                        // 사진 목록 - 가로 사이즈에 맞게 배치
-                        Row(
-                          children: reviewImages.map((imagePath) {
-                            return Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(8.0),
-                                  child: Image.asset(
-                                    imagePath,
-                                    height: 90,
-                                    fit: BoxFit.cover,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.symmetric(horizontal: 16),
+                            child: Row(
+                              children: [
+                                Text(
+                                  '${reviewData.mtId.toString().substring(0, 2)}**********',
+                                  style: TextStyle(
+                                      color: Color(0xFF7B7B7B),
+                                      fontSize: Responsive.getFont(context, 12)),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.symmetric(horizontal: 10),
+                                  child: Text(
+                                    reviewData.rtWdate ?? "",
+                                    style: TextStyle(
+                                        color: Color(0xFF7B7B7B),
+                                        fontSize: Responsive.getFont(context, 12)),
                                   ),
                                 ),
-                              ),
-                            );
-                          }).toList(),
-                        ),
-                        const SizedBox(height: 8),
-                        GestureDetector(
-                          onTap: () {
-                            // TODO 신고 버튼 클릭시 동작
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => const ReportPage()),
-                            );
-                          },
-                          child: Text(
-                            '신고',
-                            style: TextStyle(
-                              fontSize: Responsive.getFont(context, 12),
-                              color: Colors.grey,
+                              ],
                             ),
                           ),
-                        ),
-                        Divider(thickness: 1, color: Colors.grey[300]),
-                      ],
-                    ),
-                  ),
-                ),
-              );
-            }),
+                          // TODO 실제 별점 값 넣기
+                          Container(
+                            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                            child: _ratingStars(4.5),
+                          ),
+                          // 리뷰 텍스트 두 줄로 제한하고 넘칠 경우 생략 부호 표시
+                          Container(
+                            margin: EdgeInsets.symmetric(horizontal: 16),
+                            child: Text(
+                              reviewData.rtContent ?? "",
+                              style: TextStyle(
+                                fontSize: Responsive.getFont(context, 14),
+                                color: Colors.black
+                              ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 10, bottom: 15, left: 16),
+                            child: GestureDetector(
+                              onTap: () {
+                                // TODO 신고 버튼 클릭시 동작
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => const ReportPage()),
+                                );
+                              },
+                              child: Text(
+                                '신고',
+                                style: TextStyle(
+                                  fontSize: Responsive.getFont(context, 12),
+                                  color: Color(0xFF7B7B7B),
+                                ),
+                              ),
+                            ),
+                          ),
+                          // 사진 목록 - 가로 사이즈에 맞게 배치
+                          Container(
+                            margin: EdgeInsets.symmetric(horizontal: 16),
+                            child: Row(
+                              children: reviewImages.map((imagePath) {
+                                return Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(8.0),
+                                      child: Image.asset(
+                                        imagePath,
+                                        height: 90,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              }).toList(),
+                            ),
+                          ),
 
-            // 페이지네이션
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Row(
+                          Container(
+                              margin: EdgeInsets.symmetric(vertical: 20),
+                              child: Divider(thickness: 1, color: Color(0xFFEEEEEE))),
+                        ],
+                      ),
+                  );
+              }),
+
+              // 페이지네이션
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   IconButton(
@@ -254,7 +264,7 @@ class _ProductReviewState extends ConsumerState<ProductReview> {
                           '${currentPage.toString().padLeft(2, '0')}',
                           style: TextStyle(
                             fontSize: Responsive.getFont(context, 16),
-                              fontWeight: FontWeight.w600,
+                            fontWeight: FontWeight.w600,
                           ),
                         ),
                         Text(
@@ -262,8 +272,7 @@ class _ProductReviewState extends ConsumerState<ProductReview> {
                           style: TextStyle(
                               fontSize: Responsive.getFont(context, 16),
                               color: Color(0xFFCCCCCC),
-                              fontWeight: FontWeight.w600
-                          ),
+                              fontWeight: FontWeight.w600),
                         ),
                       ],
                     ),
@@ -280,8 +289,8 @@ class _ProductReviewState extends ConsumerState<ProductReview> {
                   ),
                 ],
               ),
-            ),
-          ],
+            ],
+          ),
         );
       }
     );
